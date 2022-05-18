651b80d3fca6959434bc996fa6dab89936901523
guisantosalves <workguisantos@gmail.com>
2022-05-17 22:17:46 -0400

    continuando página de cadastro

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Form, Row, Col } from "react-bootstrap";
+import { Container, Form, Row, Col, Image } from "react-bootstrap";
 import Aside from "./Aside";
 import Footer from "./Footer";
 
@@ -7,39 +7,61 @@ const Register = (props) => {
   return (
     <>
       <Aside />
-      <Container>
-        <Row>
-          <Col style={{height: "900px"}}>
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicPokemonName">
-                <Form.Label>Insira o nome do pokemon</Form.Label>
-                <Form.Control type="text" placeholder="Ex: Pikachu" />
-              </Form.Group>
+      <div style={style.Container}>
+        <Container>
+          <Row style={{border: "1px solid red", padding: "15px"}}>
+            <Col style={{ height: "700px" }}>
+              <Form>
+                <Form.Group className="mb-3" controlId="formBasicPokemonName">
+                  <Form.Label>Insira o nome do pokemon</Form.Label>
+                  <Form.Control type="text" placeholder="Ex: Pikachu" />
+                </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPokemonName">
-                <Form.Label>Insira sua descrição</Form.Label>
-                <Form.Control type="text" placeholder="Ex: É fofinho" />
-              </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPokemonName">
+                  <Form.Label>Insira sua descrição</Form.Label>
+                  <Form.Control type="text" placeholder="Ex: É fofinho" />
+                </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPokemonName">
-                <Form.Label>Insira sua força</Form.Label>
-                <Form.Control type="number" placeholder="Ex: É fofinho" />
-              </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPokemonName">
+                  <Form.Label>Insira sua força</Form.Label>
+                  <Form.Control type="number" placeholder="Ex: 2300" />
+                </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPokemonName">
-                <Form.Label>Insira sua defesa</Form.Label>
-                <Form.Control type="number" placeholder="Ex: É fofinho" />
-              </Form.Group>
-            </Form>
-          </Col>
-          <Col>
-            <h1>eaeaea</h1>
-          </Col>
-        </Row>
-      </Container>
-      <Footer/>
+                <Form.Group className="mb-3" controlId="formBasicPokemonName">
+                  <Form.Label>Insira sua defesa</Form.Label>
+                  <Form.Control type="number" placeholder="Ex: 2500" />
+                </Form.Group>
+              </Form>
+            </Col>
+            <Col>
+              <div>
+                <h1 style={style.Texts}>Seu card</h1>
+              </div>
+              <div>
+                <p style={style.Texts}>
+                  Seu Card será cadastrado e ficará amostra para todos usuários
+                  que irão acessar o site, obrigado por sua contribuição :D
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <Footer />
     </>
   );
 };
 
+const style = {
+  Texts: {
+    textAlign: "center",
+  },
+  Container: {
+    marginTop: "13px",
+    backgroundImage: `url("https://wallpapercave.com/wp/WDmLU1K.jpg")`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "auto",
+    backgroundPosition: "center",
+  },
+};
 export default Register;
